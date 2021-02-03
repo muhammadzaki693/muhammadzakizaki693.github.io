049fbdf4250aab8d6819366b5afee1027f814c12
muhammadzaki693 <75458189+muhammadzaki693@users.noreply.github.com>
2021-02-03 07:51:57 +0700

    Create connect.php

---
+++ php/connect.php
@@ -0,0 +1,9 @@
+<?php
+$server = "localhost";
+$name = "root"
+$password = "root"
+$conn = new mysqli($server, $name, $password)
+if($conn->connect_error){
+die("uncconected: $conn->connect_error"
+}
+?>
